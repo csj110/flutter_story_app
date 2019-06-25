2d24ab730912520b1fdf11850409f21a02625d60
csj110 <32285817+csj110@users.noreply.github.com>
2019-06-25 22:44:47 +0800

    2

--- lib/main.dart
+++ lib/main.dart
@@ -126,14 +126,13 @@ class _MyHomePageState extends State<MyHomePage> {
                 CardScrollWidget(currentPage),
                 Positioned.fill(
                   child: PageView.builder(
-                    itemCount: images.length,
-                    controller: controller,
-                    reverse: true,
-                    pageSnapping: true,
-                    itemBuilder: (context, index) {
-                      return Container();
-                    },
-                  ),
+                      itemCount: images.length,
+                      controller: controller,
+                      reverse: true,
+                      pageSnapping: true,
+                      itemBuilder: (context, index) {
+                        return Container();
+                      }),
                 )
               ],
             )
@@ -147,11 +146,11 @@ class _MyHomePageState extends State<MyHomePage> {
 class CardScrollWidget extends StatelessWidget {
   var currentPage;
   var padding = 20.0;
-  var veritalInset=20.0;
+  var veritalInset = 20.0;
   CardScrollWidget(this.currentPage);
   @override
   Widget build(BuildContext context) {
-    return new AspectRatio(
+    return AspectRatio(
       aspectRatio: widgetAspectRatio,
       child: LayoutBuilder(
         builder: (context, constraints) {
@@ -159,24 +158,83 @@ class CardScrollWidget extends StatelessWidget {
           var height = constraints.maxHeight;
 
           var safeWidth = width - 2 * padding;
-          var safeHeight=height-2*padding;
+          var safeHeight = height - 2 * padding;
 
-          var heightOfPrimaryCard=safeHeight;
-          var widthOfPrimaryCard=heightOfPrimaryCard*cardAspectRatio;
+          var heightOfPrimaryCard = safeHeight;
+          var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
 
-          var primaryCardLeft=safeWidth-widthOfPrimaryCard;
-          var horizontalInset=primaryCardLeft/2;
+          var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+          var horizontalInset = primaryCardLeft / 2;
 
-          for(var i=0;i < images.length;i++){
-            var delta=i-currentPage;
-            var cardItem=Positioned.directional(
-              top: padding-veritalInset*max(-delta,0.0),
-              bottom: padding+veritalInset*max(-delta,0.0),
-              start: ,
+          List<Widget> cardList = new List();
+          for (var i = 0; i < images.length; i++) {
+            var delta = i - currentPage;
+            bool isOnRight = delta > 0;
+            var start = padding +
+                max(
+                    primaryCardLeft -
+                        horizontalInset * -delta * (isOnRight ? 15 : 1),
+                    0.0);
+            var cardItem = Positioned.directional(
+              top: padding + veritalInset * max(-delta, 0.0),
+              bottom: padding + veritalInset * max(-delta, 0.0),
+              start: start,
+              textDirection: TextDirection.rtl,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Container(
+                  decoration: BoxDecoration(boxShadow: [
+                    BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(3.0, 6.0),
+                        blurRadius: 10.0)
+                  ]),
+                  child: AspectRatio(
+                    aspectRatio: cardAspectRatio,
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: <Widget>[
+                        Image.asset(images[i], fit: BoxFit.cover),
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment:CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical:8.0,horizontal: 16.0),
+                                child: Text(title[i],style:TextStyle(color: Colors.white,fontSize: 20.0)),
+                              ),
+                              SizedBox(height:10.0),
+                              Padding(
+                                padding: const EdgeInsets.only(left:12.0,bottom:12.0),
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(horizontal: 22.0,vertical: 6.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.blueAccent,
+                                    borderRadius: BorderRadius.circular(20.0)
+                                  ),
+                                  child: Text('Read Later',style:TextStyle(color: Colors.white)),
+                                  ),
+                              ),
+                              
+                            ],
+                          ),
+                        
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             );
+            cardList.add(cardItem);
           }
+          return Stack(
+            children: cardList,
+          );
         },
       ),
     );
   }
 }
+
